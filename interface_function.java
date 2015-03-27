0b84116c9646a657266c372e3fd37e5f89992b35
aegarnett <agarnett@uncc.edu>
2015-03-27 11:49:37 -0400

    Alomst finished functions for back end

--- interface_function.java
+++ interface_function.java
@@ -18,5 +18,34 @@ public class DatabaseInterface {
         Connection conn = null;
         Statement stmnt = null;
 
+
+        //Register JDBC driver
+        Class.forname("com.mysql.jdbc.Driver");
+
+        //Create the connection to the database.
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        addEntryToTable(conn,"test","test_category",25,25);
+
+    }
+
+    public static void addEntryToTable( Connection conn,String[] eventname, String[] category, int population, float timestamp){
+
+        //Execute the  entry in the approrpriate statement
+
+
+        String query = "INSERT INTO  event_default_category(Event_Name,Population,timestamp)" + "(?,?,?)";
+
+
+        //Create prepared statements for the string query.
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, eventname);
+        preparedStatement.setString(2,  population);
+        preparedStatement.setString(3, timestamp);
+        preparedStatement.executeUpdate();
+
+       //Determine Table name
+
     }
 }
